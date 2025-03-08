b82370a90b1c40b2a6669e8c18c51da7107a8a68
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-8.local>
2025-03-08 23:36:30 +0530

    init

---
+++ mytool.rb
@@ -0,0 +1,12 @@
+
+class Mytool < Formula
+  desc "A simple shell script to run stopwatch in terminal"
+  homepage "https://github.com/anishakd4/terminalstopwatch"
+  url "https://github.com/anishakd4/terminalstopwatch/releases/download/v1.0.0/anish.sh" # Update URL
+  sha256 "b6ec344c402f22ca71c6935d3e09092744a097676b08f29be1a63714c6574d22" # Use `shasum -a 256 mytool.sh` to get this
+  version "1.0.0"
+
+  def install
+    bin.install "mytool.sh" => "mytool"
+  end
+end
