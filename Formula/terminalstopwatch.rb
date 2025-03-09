ed9990d9f0f49e74584e754544a49ae1ab0678c6
Anish kumar dubey <anishkumardubey@Anishs-MacBook-Pro-8.local>
2025-03-09 08:42:04 +0530

    init

--- Formula/terminalstopwatch.rb
+++ Formula/terminalstopwatch.rb
@@ -2,9 +2,9 @@
 class Terminalstopwatch < Formula
   desc "A simple shell script to run stopwatch in terminal"
   homepage "https://github.com/anishakd4/terminalstopwatch"
-  url "https://github.com/anishakd4/terminalstopwatch/releases/download/v1.0.0/anish.sh" # Update URL
-  sha256 "b6ec344c402f22ca71c6935d3e09092744a097676b08f29be1a63714c6574d22" # Use `shasum -a 256 mytool.sh` to get this
-  version "1.0.0"
+  url "https://github.com/anishakd4/terminalstopwatch/archive/refs/tags/v2.0.0.tar.gz" # Update URL
+  sha256 "1a41c273a797e17fbe2103df3c60a64f75aeb1236b59f27f5c2372ee2650ab16" # Use `shasum -a 256 mytool.sh` to get this
+  version "2.0.0"
 
   def install
     bin.install "anish.sh" => "terminalstopwatch"
